608b9950f91df6baeeaae5fa4d7d4cbaa19f6c22
Uku Taht <uku.taht@gmail.com>
2021-05-19 16:16:17 +0300

    Truncate IP addresses

--- lib/plausible_web/remote_ip.ex
+++ lib/plausible_web/remote_ip.ex
@@ -3,17 +3,33 @@ defmodule PlausibleWeb.RemoteIp do
     cf_connecting_ip = List.first(Plug.Conn.get_req_header(conn, "cf-connecting-ip"))
     forwarded_for = List.first(Plug.Conn.get_req_header(conn, "x-forwarded-for"))
 
-    cond do
-      cf_connecting_ip ->
-        cf_connecting_ip
+    ip =
+      cond do
+        cf_connecting_ip ->
+          cf_connecting_ip
 
-      forwarded_for ->
-        String.split(forwarded_for, ",")
-        |> Enum.map(&String.trim/1)
-        |> List.first()
+        forwarded_for ->
+          String.split(forwarded_for, ",")
+          |> Enum.map(&String.trim/1)
+          |> List.first()
 
-      true ->
-        to_string(:inet_parse.ntoa(conn.remote_ip))
+        true ->
+          to_string(:inet_parse.ntoa(conn.remote_ip))
+      end
+
+    truncated(ip)
+  end
+
+  defp truncated(ip) do
+    case :inet_parse.address(String.to_charlist(ip)) do
+      {:ok, {b1, b2, b3, _b4}} ->
+        :inet_parse.ntoa({b1, b2, b3, 0}) |> to_string
+
+      {:ok, {b1, b2, b3, b4, b5, b6, b7, _b8}} ->
+        :inet_parse.ntoa({b1, b2, b3, b4, b5, b6, b7, 0}) |> to_string
+
+      {:error, _} ->
+        nil
     end
   end
 end
